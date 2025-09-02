4d26b02eb6459cb43c0aea8ab08a970483d792d7
Anant Jindal <anant.jindal@kredmint.com>
2025-09-02 21:30:15 +0530

    Created a product model

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title = models.TextField()
+    price = models.TextField()
+    description = models.TextField()
+    summary = models.TextField(default="This is my default summar!")
