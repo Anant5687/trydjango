a4597d1dcd490b2001453cd88257921a590c08b5
Anant Jindal <anant.jindal@kredmint.com>
2025-09-02 21:47:27 +0530

    Fixed model

--- src/products/models.py
+++ src/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    title = models.TextField()
-    price = models.TextField()
-    description = models.TextField()
+    title = models.CharField(max_length=150)
+    price = models.DecimalField(decimal_places=2, max_digits=1000000)
+    description = models.TextField(blank=True, null=True)
     summary = models.TextField(default="This is my default summar!")
