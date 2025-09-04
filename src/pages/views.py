aaac209f8d634ba6b08445f3345c4e6b53b205e1
Anant Jindal <anant.jindal@kredmint.com>
2025-09-04 10:49:40 +0530

    Created context from templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,8 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request, args, kwargs)
+    my_context = {
+        'my_text': "This is all about home page",
+        'my_number':8327498,
+        'my_list': [123, 321, 213, 32, 31]
+    }
     # return HttpResponse("<h1>Hello world!</h1>")
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', my_context)
 
 def contact_view (request,*args, **kwargs):
     return HttpResponse("<h1>Contact Page</h1>")
